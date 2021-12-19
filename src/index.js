c5218c952810a8e13de3b968f8b7a17f5e80ba7a
VincentHilti <visdvin@hilti.com>
2021-12-19 16:24:12 +0100

    add PORT number

--- src/index.js
+++ src/index.js
@@ -14,8 +14,8 @@ async function startApolloServer(typeDefs, resolvers) {
     },
   });
 
-  const { url, port } = await server.listen();
-  console.log(`
+  const { url, port } = await server.listen({port: process.env.PORT || 4000});
+   console.log(`
       🚀  Server is running
       🔉  Listening on port ${port}
       📭  Query at ${url}
